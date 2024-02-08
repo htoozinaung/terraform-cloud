502d96f0ae6a60f941b978aa77d4f5387aed7824
htoozinaung <129829943+htoozinaung@users.noreply.github.com>
2024-02-08 22:43:11 +1100

    Update variables.tf
    default subnet number to 2

--- networking/variables.tf
+++ networking/variables.tf
@@ -18,7 +18,7 @@ variable "virtual_network_address_space" {
 variable "number_of_subnets" {
     type=number
     description = "This defines the number of subnets"
-    default = 1
+    default = 2
     validation {
       condition = var.number_of_subnets < 5
       error_message = "The number of subnets must be less than 5."
